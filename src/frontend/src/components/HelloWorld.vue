8de11cdf7d611de60ec82db53b676a123d118fa6
Marco von Rosenberg <codingmarco@gmail.com>
2025-06-30 23:23:17 +0200

    Add frontend template

---
+++ src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container class="fill-height" max-width="900">
+    <div>
+      <div class="mb-8 text-center">
+        <h1 class="text-h2 font-weight-bold">
+          Stuttgart Bürgerbüro Wartezeiten
+        </h1>
+      </div>
+
+      <v-date-picker v-model="selectedDate"></v-date-picker>
+    </div>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { VDatePicker } from "vuetify/components";
+import { useDate } from "vuetify";
+
+const dateAdapter = useDate();
+
+const selectedDate = ref(new Date());
+</script>
