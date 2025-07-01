ceec55dac1a793e12c22ae3769d465748b0e290b
Marco von Rosenberg <codingmarco@gmail.com>
2025-07-01 14:43:57 +0200

    Add API container

--- src/frontend/src/components/HelloWorld.vue
+++ src/frontend/src/components/HelloWorld.vue
@@ -8,16 +8,36 @@
       </div>
 
       <v-date-picker v-model="selectedDate"></v-date-picker>
+
+      <div class="d-flex ga-md flex-wrap">
+        {{ offices }}
+      </div>
     </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch, watchEffect, onMounted } from "vue";
 import { VDatePicker } from "vuetify/components";
 import { useDate } from "vuetify";
 
 const dateAdapter = useDate();
 
 const selectedDate = ref(new Date());
+
+const offices = ref([]);
+
+onMounted(async () => {
+  // Fetch the office data from the API
+  try {
+    const response = await fetch("http://127.0.0.1:8000/offices");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    offices.value = await response.json();
+  } catch (error) {
+    console.error("Failed to fetch offices:", error);
+  }
+});
+
 </script>
